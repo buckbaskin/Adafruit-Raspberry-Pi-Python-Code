741465cd7daa7b3be4fdc453960c881cf849bf64
Buck Baskin <mobile.wbaskin@gmail.com>
2016-02-03 16:43:14 -0500

    Wheel behaves differently between py2, py3. See output files

--- Adafruit_LEDpixels/RainbowTest.py
+++ Adafruit_LEDpixels/RainbowTest.py
@@ -4,3 +4,7 @@ from Adafruit_LEDpixels import Color, Wheel
 print(Color(255,0,0))
 print(Color(0,255,0))
 print(Color(0,0,255))
+
+for j in range(0,256):
+    for i in range(25):
+        print(str(i)+', '+str(j)+', '+str(Wheel( ((i * 256 / 25) + j) % 256)))
